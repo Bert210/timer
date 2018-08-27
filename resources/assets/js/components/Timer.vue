4c19ad5a8b95e049a23161d58105257c8993a0b8
Robert Utchel <bert210@gmail.com>
2018-08-26 21:45:29 -0400

    Remove console.log function

--- resources/assets/js/components/Timer.vue
+++ resources/assets/js/components/Timer.vue
@@ -43,7 +43,6 @@ export default {
     },
     methods: {
         updateAll: function(){
-            console.log("Updating all!");
             this.updateQueueCallback();
             this.updateProcessingCallback();
             this.updateCompleteCallback();
